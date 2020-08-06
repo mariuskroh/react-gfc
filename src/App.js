cb85b5cb0dd4077db4d0f5725b58aa536c3904aa
Marius Kroh <mail@mariuskroh.de>
2020-08-06 14:18:12 +0200

    :zap: Remove clutter, add router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./GoodFastCheapStyles";
+import GoodFastCheap from "./GoodFastCheap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/:first-:second-:third"
+            render={(routeProps) => <GoodFastCheap options={routeProps} />}
+          />
+          <Redirect to="/good-fast-cheap" />
+        </Switch>
+      </div>
+    </ThemeProvider>
   );
 }
 
